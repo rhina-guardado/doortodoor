eb7c0831ec5725f97aed6d5222e31d6a2428194f
SrtaJossy <srtajos@gmail.com>
2026-04-28 21:10:37 -0600

    profile

--- .adonisjs/server/routes.d.ts
+++ .adonisjs/server/routes.d.ts
@@ -10,6 +10,7 @@ export type ScannedRoutes = {
     'agencias': { paramsTuple?: []; params?: {} }
     'welcome': { paramsTuple?: []; params?: {} }
     'chat': { paramsTuple?: []; params?: {} }
+    'profile': { paramsTuple?: []; params?: {} }
     'signup': { paramsTuple?: []; params?: {} }
     'new_account.store': { paramsTuple?: []; params?: {} }
     'session.create': { paramsTuple?: []; params?: {} }
@@ -23,6 +24,7 @@ export type ScannedRoutes = {
     'agencias': { paramsTuple?: []; params?: {} }
     'welcome': { paramsTuple?: []; params?: {} }
     'chat': { paramsTuple?: []; params?: {} }
+    'profile': { paramsTuple?: []; params?: {} }
     'signup': { paramsTuple?: []; params?: {} }
     'session.create': { paramsTuple?: []; params?: {} }
   }
@@ -33,6 +35,7 @@ export type ScannedRoutes = {
     'agencias': { paramsTuple?: []; params?: {} }
     'welcome': { paramsTuple?: []; params?: {} }
     'chat': { paramsTuple?: []; params?: {} }
+    'profile': { paramsTuple?: []; params?: {} }
     'signup': { paramsTuple?: []; params?: {} }
     'session.create': { paramsTuple?: []; params?: {} }
   }
